3d4a1f9f6b8422cd7f6557166eb2cd3bd78bac8b
thakurakhilesh1998 <39956027+thakurakhilesh1998@users.noreply.github.com>
2020-09-08 20:39:16 +0530

    final

--- src/App.js
+++ src/App.js
@@ -2,29 +2,54 @@ import React,{useState} from 'react';
 import './index.css';
 
 const App=()=>{
-const [name,setName]=useState();
-const [lastName,setLastName]=useState('');
-const [fullanme,setFullName]=useState('');
+const [details,setDetails]=useState({fname:'',lname:'',email:''});
 
   const onInputTyped=(event)=>{
-    setName(event.target.value);
-  }
 
-  const onLastNameTyped=(event)=>{
-    setLastName(event.target.value);
+      const name=event.target.name;
+      console.log(name);
+      const value=event.target.value;
+    setDetails((pre)=>{
+    
+      switch(name)
+      {
+        case("fname"):
+        return{
+        fname:value,
+        lname:pre.lastName,
+        email:pre.email
+          };
+        case("lname"):
+        return{
+          fname:pre.fname,
+          lname:value,
+          email:pre.email
+        };
+        case("email"):
+        return{
+        fname:pre.fname,
+        lname:pre.lname,
+        email:value,
+      }
+
+      }
+
+    });
   }
   const getName=(e)=>{
     e.preventDefault();
-    setFullName(`${name} ${lastName}`);
+    alert("form submitted successfully");
   }
 
   return(
       <>
         <div>
           <form className="form" onSubmit={getName}>
-            <h1>Hello {fullanme}</h1>
-            <input type="text" className="name" placeholder="First Name" value={name} onChange={onInputTyped}></input>
-             <input type="text" className="name" placeholder="Last Name" value={lastName} onChange={onLastNameTyped}></input>
+            <h1>Hello {details.fname} <span>{details.lname}</span></h1>
+            <h1 id='email'>{details.email}</h1>
+            <input type="text" className="name" placeholder="First Name" name="fname" value={details.fname} onChange={onInputTyped}></input>
+             <input type="text" className="name" placeholder="Last Name" name="lname" value={details.lname} onChange={onInputTyped}></input>
+             <input type="text" className="name" placeholder="Email Address" name="email" value={details.email} onChange={onInputTyped}></input>
             <button id="btn" type="submit">Get Name</button>
           </form>
         </div>
