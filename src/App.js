a5b1f1ff59ea487ce64c417ebecdb15191b08f2c
thakurakhilesh1998 <39956027+thakurakhilesh1998@users.noreply.github.com>
2020-09-08 21:00:43 +0530

    add mobile field

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React,{useState} from 'react';
 import './index.css';
 
 const App=()=>{
-const [details,setDetails]=useState({fname:'',lname:'',email:''});
+const [details,setDetails]=useState({fname:'',lname:'',email:'',phone:''});
 
   const onInputTyped=(event)=>{
 
@@ -17,20 +17,31 @@ const [details,setDetails]=useState({fname:'',lname:'',email:''});
         return{
         fname:value,
         lname:pre.lname,
-        email:pre.email
+        email:pre.email,
+        phone:pre.phone,
           };
         case("lname"):
         return{
           fname:pre.fname,
           lname:value,
-          email:pre.email
+          email:pre.email,
+          phone:pre.phone,
         };
         case("email"):
         return{
         fname:pre.fname,
         lname:pre.lname,
         email:value,
+        phone:pre.phone,
       }
+
+        case("phone"):
+        return{
+          fname:pre.fname,
+          lname:pre.lname,
+          email:pre.email,
+          phone:value,
+        };
 
       }
 
@@ -46,10 +57,12 @@ const [details,setDetails]=useState({fname:'',lname:'',email:''});
         <div>
           <form className="form" onSubmit={getName}>
             <h1>Hello {details.fname} {details.lname}</h1>
-            <h1 id='email'>{details.email}</h1>
+            <h1 className='email'>{details.email}</h1>
+            <h1 className='email'>{details.phone}</h1>
             <input type="text" className="name" placeholder="First Name" name="fname" value={details.fname} onChange={onInputTyped}></input>
              <input type="text" className="name" placeholder="Last Name" name="lname" value={details.lname} onChange={onInputTyped}></input>
-             <input type="text" className="name" placeholder="Email Address" name="email" value={details.email} onChange={onInputTyped}></input>
+             <input type="email" className="name" placeholder="Email Address" name="email" value={details.email} onChange={onInputTyped}></input>
+             <input type="number" className="name" placeholder="Mobile Number" name="phone" value={details.phone} onChange={onInputTyped}></input>
             <button id="btn" type="submit">Get Name</button>
           </form>
         </div>
